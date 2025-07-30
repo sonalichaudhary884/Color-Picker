0ec0dafc42c7d236f8473916780c5d8a0ab34f46
sonalichaudhary844@gmail.com <sonalichaudhary884>
2025-07-30 22:18:21 +0530

    colors

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,17 @@ import "./Card.css";
 
 const colors = ["#FF6B6B", "#6BCB77", "#4D96FF", "#FFC300", "#C74B50"];
 const Card = () => {
-  return <div className="card"></div>;
+  return (
+    <div className="card">
+      {colors.map((color, index) => (
+        <div
+          key={index}
+          style={{ backgroundColor: color }}
+          className="color-box"
+        ></div>
+      ))}
+    </div>
+  );
 };
 
 export default Card;
