29e2c9b1b2c9d1a8ec0febe5f0ee3ff84efdf6c6
sonalichaudhary844@gmail.com <sonalichaudhary884>
2025-08-14 21:45:48 +0530

    bvn

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,12 @@
 import React from "react";
 import "./Card.css";
 
-const colors = ["#FF6B6B", "#6BCB77", "#4D96FF", "#FFC300", "#C74B50"];
+const colors = [
+  ["red", "yellow", "green", "purple", "brown"],
+  ["teal", "blue", "green", "black", "brown"],
+  ["cyan", "yellow", "blue", "gray", "brown"],
+  ["cyan", "yellow", "brown", "red", "brown"],
+];
 const Card = () => {
   return (
     <div className="card">
